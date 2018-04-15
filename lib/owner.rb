aa4271a670baad3850ac297302442d1b3c2cb9c7
David McGrann <dpmcgrann@gmail.com>
2018-04-15 18:38:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,16 +1,9 @@
 require 'pry'
 class Owner
   binding.pry
-  attr_accessor :name, :pets
   @@all = []
-
   def self.all
     @@all
-  end
-  
-  def initialize(name)
-    @name = name
-    @pets = {fishes: [], cats: [], dogs: []}
   end
 
 end
