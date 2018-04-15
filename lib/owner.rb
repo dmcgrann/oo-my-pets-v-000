adfa9b9d09a549136590e5f45b32ba28a87bba1e
David McGrann <dpmcgrann@gmail.com>
2018-04-15 19:08:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,14 @@
 require 'pry'
 class Owner
   attr_accessor :pets
+  attr_reader :spieces
   @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all = self
+    @pets = {:cats => [], :dogs => [], :fishes => []}
+  end
 
   def self.all
     @@all
