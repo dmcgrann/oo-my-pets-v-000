0873ef4fe0bb08a81238df90cdcfe31298058673
David McGrann <dpmcgrann@gmail.com>
2018-04-15 18:47:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,14 @@
 require 'pry'
 class Owner
   attr_accessor :pets
+  @@owners = []
 
   def initialize(pets)
     @pets = {fishes: [], cats: [], dogs: []}
   end
 
-  def self.reset_all
-    self.count += 0
+  def self.all
+    @@owners
   end
 
 end
